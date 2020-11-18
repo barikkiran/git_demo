8d2d4ae0b5af9fdb5b4225e4d97fe6e36c8b60a5
kiran <kkbarik2031@gmail.com>
2020-11-18 09:25:22 +0530

    adding git_demo project to my local repo

---
+++ src/main/java/com/git/basic/example/AccountUtility.java
@@ -0,0 +1,16 @@
+package com.git.basic.example;
+
+public class AccountUtility {
+
+	
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hi Good Morning...");
+		
+		
+	}
+	
+	
+}

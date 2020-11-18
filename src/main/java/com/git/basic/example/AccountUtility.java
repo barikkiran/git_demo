5f81c893996014ddc55485723202b29936b14399
kiran <kkbarik2031@gmail.com>
2020-11-18 09:41:38 +0530

    Adding if and else condition in AccountUtility.java class

--- src/main/java/com/git/basic/example/AccountUtility.java
+++ src/main/java/com/git/basic/example/AccountUtility.java
@@ -3,6 +3,16 @@ package com.git.basic.example;
 public class AccountUtility {
 
 	
+	public void checkInput(String name) {
+		
+		if(name.equals("NSE"))
+			System.out.println("NSE USER");
+		else
+			System.out.println("CENTRAL USER");
+		
+	}
+	
+	
 	
 	public static void main(String[] args) {
 		
